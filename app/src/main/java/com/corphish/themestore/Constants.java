48e65d2311dcd92ec08f4fffbf60eda1d9f50862
Avinaba Dalal <d97.avinaba@gmail.com>
2016-10-13 18:14:30 +0530

    ThemeStore: Add constants

---
+++ app/src/main/java/com/corphish/themestore/Constants.java
@@ -0,0 +1,17 @@
+package com.corphish.themestore;
+
+/**
+ * Created by Avinaba on 10/13/2016.
+ */
+public class Constants {
+    // Holds all constant stuff. Easily modify it from here.
+
+    // Log tag
+    public static String LOG_TAG = "ThemeStore";
+
+    // UI
+    public static int CARDVIEW_MARGIN           = 16;
+    public static float CARDVIEW_ELEVATION      = 4;
+
+    public static int IMAGEVIEW_DIMENSION       = 64;
+}
