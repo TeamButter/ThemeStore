4b6d2edcf4df3b640fe06a4533fa0655fa88e5ae
Avinaba Dalal <d97.avinaba@gmail.com>
2016-10-13 19:07:14 +0530

    ThemeStore: StoreUI: Initial bringup

--- app/src/main/java/com/corphish/themestore/StoreUI.java
+++ app/src/main/java/com/corphish/themestore/StoreUI.java
@@ -1,13 +1,18 @@
 package com.corphish.themestore;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.support.v7.widget.CardView;
+import android.widget.TextView;
 
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
+import java.util.ArrayList;
 
 /**
  * Created by Avinaba on 10/12/2016.
@@ -18,10 +23,13 @@ public class StoreUI {
     Context context;
 
     // Layout - on which master layout you will add your child views
-    LinearLayout layout;
+    LinearLayout layout = null;
 
     // Linearlayout Param - We define this so that we dont have to define it further.
     LinearLayout.LayoutParams params;
+
+    // Themes ArrayList - The list of themes will be fetched from server as json and parsed as Themes'Arraylist, which will be processed here.
+    ArrayList<Theme> themeArrayList = null;
 
     // Constructor - Initialize storeUI with this constructor, passing context as arg
     public StoreUI(Context context) {
@@ -31,6 +39,10 @@ public class StoreUI {
     // After initialization, set the layout using this function
     public void setLayout(LinearLayout layout) {
         this.layout = layout;
+    }
+
+    public void setThemeArrayList(ArrayList<Theme> themeArrayList) {
+        this.themeArrayList = themeArrayList;
     }
 
     public void addToParent(View v) {
@@ -68,7 +80,70 @@ public class StoreUI {
         return linearLayout;
     }
 
-    public void init() {
+    public TextView createTextView(String string) {
+        TextView textView = new TextView(context);
+        textView.setTypeface(null, Typeface.BOLD);
+
+        textView.setPadding(Constants.TEXTVIEW_PADDING,Constants.TEXTVIEW_PADDING,Constants.TEXTVIEW_PADDING,Constants.TEXTVIEW_PADDING);
+
+        return textView;
+    }
+
+    public void createThemeCard(Theme theme) {
+        CardView cardView = createCardView();
+
+        LinearLayout horizontalLayout = createLayout(LinearLayout.HORIZONTAL);
+
+        ImageView imageView = createImage();
+        syncImage(imageView,theme.getThumbnailUrl());
+
+        TextView textView = createTextView(theme.getName());
+
+        horizontalLayout.addView(imageView);
+        horizontalLayout.addView(textView);
+
+        cardView.addView(horizontalLayout);
+
+        addToParent(cardView);
+    }
+
+    public void createPage(ArrayList<Theme> themes) {
+        for(int i = 0; i < themes.size(); i++) createThemeCard(themes.get(i));
+    }
+
+    public int init() {
+
+        int warn_count = 0;
+
+        Log.i(Constants.LOG_TAG,"Initializing Store UI...");
+
+        // Init layout params
         params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // Check whether parent layout is defined or not
+        if(layout == null) {
+            Log.e(Constants.LOG_TAG,"Parent layout not defined. It must be defined using setLayout(LinearLayout).");
+            return Constants.STATUS_FATAL;
+        }
+
+        // Check whether ThemeArray is init or not
+        if(themeArrayList == null) {
+            Log.e(Constants.LOG_TAG,"ThemeLists not defined. It must be defined using setThemeArrayList(ArrayList<Theme>)");
+            return Constants.STATUS_FATAL;
+        }
+
+        // Check whether themeArrayList is empty or not
+        if(!themeArrayList.isEmpty()) {
+            Log.w(Constants.LOG_TAG,"ThemeList defined but empty.");
+            warn_count++;
+        }
+
+        Log.i(Constants.LOG_TAG,"StoreUI initialized" + (warn_count == 0 ? "." : " with warnings."));
+
+        // Now that we are good to go, show the themes to user
+        Log.i(Constants.LOG_TAG,"Showing themes...");
+        createPage(themeArrayList);
+
+        return Constants.STATUS_OK;
     }
 }
