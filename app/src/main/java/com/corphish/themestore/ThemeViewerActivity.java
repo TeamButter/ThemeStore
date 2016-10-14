9447dbc72a4842b030cbe98925b78f3ce5e147d4
Avinaba Dalal <d97.avinaba@gmail.com>
2016-10-14 23:33:07 +0530

    ThemeStore: ThemeViewer: Initial bringup

--- app/src/main/java/com/corphish/themestore/ThemeViewerActivity.java
+++ app/src/main/java/com/corphish/themestore/ThemeViewerActivity.java
@@ -1,23 +1,41 @@
 package com.corphish.themestore;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.corphish.themestore.R;
 import com.corphish.themestore.Theme;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
+import java.io.File;
 
 public class ThemeViewerActivity extends AppCompatActivity {
+
+    long downloadReferenceId;
+    Theme theme;
+
+    DownloadManager downloadManager;
+    BroadcastReceiver downloadReciever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +43,7 @@ public class ThemeViewerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_theme_viewer);
 
         Intent intent = getIntent();
-        final Theme theme = (Theme)intent.getSerializableExtra("Theme");
+        theme = (Theme)intent.getSerializableExtra("Theme");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,16 +51,35 @@ public class ThemeViewerActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
         collapsingToolbarLayout.setExpandedTitleColor(Color.TRANSPARENT);
 
+
+        downloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+
         setTitle(theme.getName());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Uri uri = Uri.parse(theme.getDownloadUrl());
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+
+                request.setTitle(theme.getName());
+                request.setDescription("StryFlex ROM Theme");
+
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+
+                request.setDestinationInExternalFilesDir(ThemeViewerActivity.this, Environment.DIRECTORY_DOWNLOADS,theme.getPackageName() + ".apk");
+
+                downloadReferenceId = downloadManager.enqueue(request);
+
+                // TODO: To update FAB when download has started, like disable the icon (grey out it).
+                fab.setClickable(false);
             }
         });
+
+        ImageView imageView = (ImageView)findViewById(R.id.banner_image);
+        UrlImageViewHelper.setUrlDrawable(imageView,theme.getBannerUrl(),R.drawable.back);
 
         TextView textView_name = (TextView)findViewById(R.id.theme_name);
         textView_name.setTypeface(null, Typeface.BOLD);
@@ -52,6 +89,30 @@ public class ThemeViewerActivity extends AppCompatActivity {
         textView_desc.setGravity(Gravity.CENTER);
         textView_desc.setText(theme.getDescription());
 
+        downloadReciever = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                long referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                if(downloadReferenceId == referenceId) {
+                    String action = intent.getAction();
+                    if(DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                        String filePath = Environment.getExternalStorageDirectory() + "/Android/data/" + getApplicationContext().getPackageName() + "/files/Download/" + theme.getPackageName() + ".apk";
+                        Intent promptInstall = new Intent(Intent.ACTION_VIEW)
+                                .setDataAndType(Uri.fromFile(new File(filePath)),
+                                        "application/vnd.android.package-archive");
+                        Log.i(Constants.LOG_TAG,"To install - " + Uri.fromFile(new File(filePath)));
+                        startActivity(promptInstall);
+                    }
+                }
+            }
+        };
 
+        registerReceiver(downloadReciever, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(downloadReciever);
     }
 }
